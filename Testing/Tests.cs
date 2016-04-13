479b5560a60c64608f43c8cd9bae3e17a9f19409
Alistair Woodcock <alistairwoodcock@gmail.com>
2016-04-13 11:41:51 +1000

    added testing with NUnit

---
+++ Testing/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Fleet;
+
+namespace NunitTests
+{
+    [TestFixture]
+    public class FleetUnitTests
+    {
+        private MainClass mainClass;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            mainClass = new MainClass();
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            mainClass = null;
+        }
+
+        [Test]
+        public void TestKillAllHumans()
+        {
+            int result = mainClass.killAllHumans();
+            Assert.AreEqual(5, result);
+        }
+        
+    }
+}

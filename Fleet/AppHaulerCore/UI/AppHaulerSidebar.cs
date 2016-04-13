2c8566183cbd3f71698392ec92798ee354d320db
unknown <tnew2294@gmail.com>
2016-04-13 12:54:41 +1000

    Set up GTk in proj. Misses DLL at runtime

---
+++ Fleet/AppHaulerCore/UI/AppHaulerSidebar.cs
@@ -0,0 +1,48 @@
+﻿using Fleet.AppHaulerCore.UI.Interfaces;
+
+using Gtk;
+
+namespace Fleet.AppHaulerCore.UI
+{
+    public class AppHaulerSidebar : IComponent
+    {
+
+        private Window sidebarWindow { get; set; }
+
+        public AppHaulerSidebar()
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Construct()
+        {
+            sidebarWindow = new Window("AppHauler");
+            sidebarWindow.Resize(200,200); // Read from configuration
+
+            // Create a demo label
+            var label = new Label("Test Label");
+            sidebarWindow.Add(label);
+        }
+
+        /// <summary>
+        /// Launches the SIdebacr component
+        /// </summary>
+        public void Launch()
+        {
+            sidebarWindow.ShowAll();
+            Gtk.Application.Run();
+        }
+
+        /// <summary>
+        /// Initializes Gtk and the Sidebar component
+        /// </summary>
+        public void Initialize()
+        {
+            Gtk.Application.Init();
+        }
+
+    }
+}
---
+++ Fleet/AppHaulerCore/UI/Interfaces/IComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fleet.AppHaulerCore.UI.Interfaces
+{
+    interface IComponent
+    {
+        /// <summary>
+        /// Initialize a UI component, accesssing and setting any needed config
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Launch the UI component doing and required rendering operations
+        /// </summary>
+        void Launch();
+
+        /// <summary>
+        /// Construct the UI component
+        /// </summary>
+        void Construct();
+
+    }
+}

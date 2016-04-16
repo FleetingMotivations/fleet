7118698663546897bbc66e110065e9c202c0b16e
Alistair Woodcock <alistairwoodcock@gmail.com>
2016-04-16 21:11:49 +1000

    added more examples and changed solution config for NUnit

--- Testing/FleetTests.cs
+++ Testing/FleetTests.cs
@@ -31,6 +31,39 @@ namespace NunitTests
             int result = mainClass.killAllHumans();
             Assert.AreEqual(5, result);
         }
+
+        [Test]
+        public void ArrayExample()
+        {
+            int[] array = new int[] { 1, 2, 3 };
+            Assert.That(array, Has.Exactly(1).EqualTo(3));
+            Assert.That(array, Has.Exactly(2).GreaterThan(1));
+            Assert.That(array, Has.Exactly(3).LessThan(100));
+        }
+
+        [Test, Combinatorial]
+        public void CombinatorialExample(
+            [Values(1,2,3)] int x,
+            [Values("First String","Second String")] string s)
+        {
+            /*
+             * 1, "First String"
+             * 2, "First String"
+             * 3, "First String"
+             * 1, "Second String"
+             * 2, "Second String"
+             * 3, "Second String"
+             */
+
+            Assert.That(x + x, Is.Not.EqualTo(5));
+            Assert.That(s, Is.Not.EqualTo("Third String"));
+        }
+
+        [Test]
+        public void ConstraintExample()
+        {
+            Assert.That(2.3, Is.GreaterThan(2.0).And.LessThan(3.0));
+        }
         
     }
 }

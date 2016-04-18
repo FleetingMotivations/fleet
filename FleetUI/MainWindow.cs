dcccb80e5e71ebd3e51074f712196bf4eb94b0b4
unknown <tnew2294@gmail.com>
2016-04-18 16:18:32 +1000

    Set up basic UI project for creating UI components

---
+++ FleetUI/MainWindow.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Gdk;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+
+	protected void OnOpen (object sender, EventArgs e)
+	{
+        var fileDialog = new FileChooserDialog (
+            "Choose an Image to View",
+            this,
+            FileChooserAction.Open,
+            "Cancel",
+            ResponseType.Cancel,
+            "Open",
+            ResponseType.Accept
+        );
+
+	    if (fileDialog.Run() == (int) ResponseType.Accept)
+	    {
+	        var fileExtension = fileDialog.Filename.Split('.')
+                .LastOrDefault();
+	        var types = new [] {"png", "gif", "jpg"};
+
+	        if (types.Contains(fileExtension))
+	        {
+			    displayImage?.Pixbuf?.Dispose();
+	            displayImage.Pixbuf = new Pixbuf(fileDialog.Filename);
+	        }
+
+	        fileDialog.Destroy();
+	    }
+	}
+
+
+	protected void OnExit (object sender, EventArgs e)
+	{
+		Application.Quit();
+	}
+
+
+	protected void OnAbout (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}
+
+}

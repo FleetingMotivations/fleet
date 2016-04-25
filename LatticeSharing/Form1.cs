6373283863a5e5250b81f0f95e5827b7ae039f42
= <hayden.cheers@me.com>
2016-04-25 17:27:29 +1000

    Basic image sharing app. Not pretty. Successfuly shares images accross network.

---
+++ LatticeSharing/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using Fleet.Lattice.Network;
+
+namespace LatticeSharing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectImage(object sender, MouseEventArgs e)
+        {
+            var open = new OpenFileDialog();
+            open.Filter = "Image Files (*.jpg, *.jpeg) | *.jpg; *.jpeg;";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                var image = Image.FromFile(open.FileName);
+                this.pictureBox1.Image = image;
+            }
+        }
+
+        private void ShareImage(object sender, MouseEventArgs e)
+        {
+            var selector = new SelectionForm();
+            selector.ShowDialog(this);
+
+            var image = this.pictureBox1.Image;
+
+            var targets = selector.selectedRecords;
+            foreach (var service in targets)
+            {
+                var address = new EndpointAddress("net.tcp://" + service.Hostname + "/Lattice");
+                var binding = new NetTcpBinding();
+                binding.Security.Mode = SecurityMode.None;
+
+                var client = new LatticeServiceClient(binding, address);
+                client.SendImage(image as Bitmap);
+            }
+        }
+    }
+}
---
+++ LatticeSharing/SelectionForm.cs
@@ -0,0 +1,65 @@
+﻿using Fleet.Lattice.Discovery;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LatticeSharing
+{
+    public class RepresentedListViewItem<T>: ListViewItem
+    {
+        public T RepresentedObject { get; set; }
+    }
+
+    public partial class SelectionForm : Form
+    {
+        public List<ServiceRecord> selectedRecords;
+
+        public SelectionForm()
+        {
+            InitializeComponent();
+
+            table.View = View.Details;
+            table.FullRowSelect = true;
+            table.GridLines = true;
+
+            ColumnHeader serviceName = new ColumnHeader();
+            serviceName.Text = "Service Name";
+            ColumnHeader hostName = new ColumnHeader();
+            hostName.Text = "Host name";
+
+            table.Columns.AddRange(new ColumnHeader[] { serviceName, hostName });
+
+            var records = Program.discovery.CurrentRecords;
+            
+            foreach (var recordpair in records)
+            {
+                var record = recordpair.Value;
+
+                var row = new RepresentedListViewItem<ServiceRecord>();
+                row.RepresentedObject = record;
+                row.Text = record.ServiceName;
+                row.SubItems.Add(record.Hostname);
+
+                table.Items.Add(row);
+            }
+        }
+
+        private void SelectTargets(object sender, EventArgs e)
+        {
+            var indicies = table.SelectedIndices;
+            selectedRecords = new List<ServiceRecord>();
+
+            foreach (int index in indicies)
+            {
+                var item = table.Items[index] as RepresentedListViewItem<ServiceRecord>;
+                selectedRecords.Add(item.RepresentedObject);
+            }
+        }
+    }
+}

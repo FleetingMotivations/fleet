72c3d25a4b970ff044f66d238a4d7f81c825eb6e
= <hayden.cheers@me.com>
2016-05-16 20:45:24 +1000

    removed share option

--- FleetUI/gtk-gui/MainWindow.cs
+++ FleetUI/gtk-gui/MainWindow.cs
@@ -88,7 +88,7 @@ public partial class MainWindow
 		this.vbox1.Name = "vbox1";
 		this.vbox1.Spacing = 6;
 		// Container child vbox1.Gtk.Box+BoxChild
-		this.UIManager.AddUiFromString (@"<ui><menubar name='menubar1'><menu name='FileAction' action='FileAction'><menuitem name='OpenAction' action='OpenAction'/><menuitem name='AboutAction' action='AboutAction'/><separator/><menuitem name='ExitAction' action='ExitAction'/></menu><menu name='CaptureAction' action='CaptureAction'><menuitem name='DesktopAction' action='DesktopAction'/><menuitem name='PortionAction' action='PortionAction'/></menu><menu name='TrollAction' action='TrollAction'><menuitem name='FullscreenAction' action='FullscreenAction'/><menuitem name='EnableBordersAction' action='EnableBordersAction'/></menu><menu name='ShareAction' action='ShareAction'><menuitem name='ToWorkstationsAction' action='ToWorkstationsAction'/></menu></menubar></ui>");
+		this.UIManager.AddUiFromString (@"<ui><menubar name='menubar1'><menu name='FileAction' action='FileAction'><menuitem name='OpenAction' action='OpenAction'/><menuitem name='AboutAction' action='AboutAction'/><separator/><menuitem name='ExitAction' action='ExitAction'/></menu><menu name='CaptureAction' action='CaptureAction'><menuitem name='DesktopAction' action='DesktopAction'/><menuitem name='PortionAction' action='PortionAction'/></menu><menu name='TrollAction' action='TrollAction'><menuitem name='FullscreenAction' action='FullscreenAction'/><menuitem name='EnableBordersAction' action='EnableBordersAction'/></menu></menubar></ui>");
 		this.menubar1 = ((global::Gtk.MenuBar)(this.UIManager.GetWidget ("/menubar1")));
 		this.menubar1.Name = "menubar1";
 		this.vbox1.Add (this.menubar1);
